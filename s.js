bbb825e5c572e970a741e9340af2699f860470c4
mustafa-shalaby <61481813+mustafa-shalaby@users.noreply.github.com>
2020-11-07 22:39:32 +0200

    Add files via upload

---
+++ s.js
@@ -0,0 +1,29 @@
+// var http = require('http');
+// var url = require('url');
+// var fs = require('fs');
+
+// http.createServer(function (req, res) {
+//   var q = url.parse(req.url, true);
+//   var filename = "." + q.pathname;
+//   fs.readFile(filename, function(err, data) {
+//     if (err) {
+//       res.writeHead(404, {'Content-Type': 'text/html'});
+//       return res.end("404 Not Found");
+//     } 
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(data);
+//     var myReadStream =fs.createReadStream(__dirname+'/web.html','utf8')
+//     myReadStream.pipe(res);
+//     return res.end();
+//   });
+// }).listen(8089); 
+
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('web.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8099); 
